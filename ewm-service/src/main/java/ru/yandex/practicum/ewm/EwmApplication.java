1a46ce005edbd9c9f00809cc8453d06c104bea52
Ирина <barhatov4@yandex.ru>
2024-11-27 21:45:03 +0300

    Реализовать ТЗ 2 этапа дипломного проекта...

--- ewm-service/src/main/java/ru/yandex/practicum/ewm/EwmApplication.java
+++ ewm-service/src/main/java/ru/yandex/practicum/ewm/EwmApplication.java
@@ -3,9 +3,11 @@ package ru.yandex.practicum.ewm;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"ru.practicum.client", "ru.practicum.ewm"})
 public class EwmApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(EwmApplication.class, args);
     }
+
 }
