de17cd543b6176bc849bea68832a15dd7f17ba65
Ирина <barhatov4@yandex.ru>
2024-12-01 13:08:39 +0300

    Реализовать ТЗ 2 этапа дипломного проекта.

--- ewm-service/src/main/java/ru/yandex/practicum/ewm/event/service/EventServiceImpl.java
+++ ewm-service/src/main/java/ru/yandex/practicum/ewm/event/service/EventServiceImpl.java
@@ -12,7 +12,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ru.yandex.practicum.client.StatsClient;
 import ru.yandex.practicum.dto.ParamHitDto;
